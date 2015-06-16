b5d0e037fa58c231ac4473ad3f203323388b1a19
Jung Yoon <jungyoon@utexas.edu>
2015-06-16 03:39:23 -0500

    Optimization -->  n + (n >> 1) + 1

--- Collatz.py
+++ Collatz.py
@@ -79,10 +79,12 @@ def collatz_cycle_length (l) :
     tempNum = l
     while tempNum > 1 : 
         if tempNum % 2 == 0 :
-            tempNum /= 2
+            tempNum //= 2
+            count += 1
         else :
-            tempNum = (3 * tempNum) + 1
-        count += 1
+            # tempNum = (3 * tempNum) + 1
+            tempNum += (tempNum >> 1) + 1
+            count += 2
 
     # post-conditions
     assert count > 0
